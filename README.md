e6544d0fa8edc7d2f759a7f8fde48f9dbecf3b64
JK-Chung <chungjeeken@outlook.com>
2022-04-04 19:17:50 +0100

    continuing tab -> space adventure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # routing-infrastructure
-Terraform Repo for managing Route 53, Load Balances and APIGWs across all my projects
+Terraform Repo for managing Route 53, Load Balances and APIGWs across all my projects.
--- load_balancer.tf
+++ load_balancer.tf
@@ -1,26 +1,26 @@
 resource "aws_lb" "common_lb" {
-    name = "common-load-balancer"
-    load_balancer_type = "application"
+  name = "common-load-balancer"
+  load_balancer_type = "application"
 
-    security_groups = [aws_security_group.lb_security_group.id]
-    subnets = [for s in aws_subnet.public : s.id]
+  security_groups = [aws_security_group.lb_security_group.id]
+  subnets = [for s in aws_subnet.public : s.id]
 
-    internal = false
-    enable_deletion_protection = "true"
+  internal = false
+  enable_deletion_protection = "true"
 
-    tags = local.common_tags
+  tags = local.common_tags
 }
 
 resource "aws_security_group" "lb_security_group" {
-    name = "SG_common_load_balancer"
-    description = "Security Group controlling ingress and egress for the common load balancer"
+  name = "SG_common_load_balancer"
+  description = "Security Group controlling ingress and egress for the common load balancer"
 
-    ingress {
-        description = "Enforce TLS Ingress Only"
-        from_port = "443"
-        to_port = "443"
-        protocol = "tcp"
-    }
+  ingress {
+    description = "Enforce TLS Ingress Only"
+    from_port = "443"
+    to_port = "443"
+    protocol = "tcp"
+  }
 
-    tags = local.common_tags
+  tags = local.common_tags
 }
