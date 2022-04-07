80d057df5207eba5db867899f44e054fa77e8371
JK-Chung <chungjeeken@outlook.com>
2022-04-07 22:25:20 +0100

    corrected formating

--- ssm.tf
+++ ssm.tf
@@ -1,9 +1,9 @@
 resource "aws_ssm_parameter" "public_subnet_ids" {
   count = length(aws_subnet.public)
 
-  name = format("/vpc/public_subnet_id/%d", count.index) 
-  type = "String"
-  value = aws_subnet.public[count.index].id
+  name        = format("/vpc/public_subnet_id/%d", count.index)
+  type        = "String"
+  value       = aws_subnet.public[count.index].id
   description = "The"
 
   tags = local.common_tags
