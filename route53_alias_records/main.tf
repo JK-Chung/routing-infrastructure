29526f570a4b5a68eb46049c5b5baa13b8099fc6
JK-Chung <chungjeeken@outlook.com>
2022-05-21 21:07:11 +0100

    commit

--- route53_alias_records/main.tf
+++ route53_alias_records/main.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "subdomain_alias_records" {
 
   name    = "${each.value}.${var.route53_zone_name}"
   type    = "A"
-  zone_id = var.route_53_zone_id
+  zone_id = var.route53_zone_id
 
   alias {
     name    = var.to_alias_to.name
