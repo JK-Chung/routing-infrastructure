453194a82a0fc1f59b86e2541e26cfa839b2e877
JK-Chung <chungjeeken@outlook.com>
2022-05-21 21:01:51 +0100

    Corrected

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  route53_zone_names = [for route53_zone_names in distinct(local.elb_routable_apps[*][route53_zone_names]) : route53_zone_names]
+  route53_zone_names = [ for z in distinct(local.elb_routable_apps[*][route53_zone_name]) : z ]
 
   # The code has been set up to automatically generate the target-groups, ALB listeners, TLS certificates, Route53 records for each element in this list
   # Whenever you want to onboard a new publically-routable project with a domain-name (THAT IS ROUTED THROUGH THE COMMON ALB), you add onto this list  
