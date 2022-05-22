ab2ef5c054fc61019a0a50eae6af6059ded4d2e7
JK-Chung <chungjeeken@outlook.com>
2022-05-22 13:03:07 +0100

    added ttl

--- route53_cross_account/main.tf
+++ route53_cross_account/main.tf
@@ -3,4 +3,5 @@ resource "aws_route53_record" "env_domain_name" {
   type    = "NS"
   zone_id = data.tfe_outputs.cross_account-route53.values.apex-domains-route53[var.apex_domain].zone_id
   records = var.env_nameservers
+  ttl     = 60
 }
