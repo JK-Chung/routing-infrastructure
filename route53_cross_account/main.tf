5e10d85b655e67c118de3b3141820c127a65eddb
JK-Chung <chungjeeken@outlook.com>
2022-05-22 12:32:46 +0100

    values

--- route53_cross_account/main.tf
+++ route53_cross_account/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "env_domain_name" {
   name    = var.env_domain_name
   type    = "NS"
-  zone_id = data.tfe_outputs.cross_account-route53.value[var.apex_domain].zone_id
+  zone_id = data.tfe_outputs.cross_account-route53.values[var.apex_domain].zone_id
   records = var.env_nameservers
 }
