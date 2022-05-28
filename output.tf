34bca7f5eaf9838c6de60e26b1f9acf75a984cb2
JK-Chung <chungjeeken@outlook.com>
2022-05-28 17:56:32 +0100

    Correct output

--- output.tf
+++ output.tf
@@ -1,6 +1,4 @@
 output "project-nameservers" {
-  for_each    = local.env_root_domain
   description = "Declares the nameservers for each project's environment root domains"
-
-  value = { for d in each.value : d => aws_route53_zone.projects[d].name_servers }
+  value = { for d in local.env_root_domain : d => aws_route53_zone.projects[d].name_servers }
 }
