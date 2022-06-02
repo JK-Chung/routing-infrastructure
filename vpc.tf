06d1fd1bf489eca49d2e82a4e6874541a55c6f89
JK-Chung <chungjeeken@outlook.com>
2022-06-02 19:50:07 +0100

    Added VPC Endpoint for DynamoDB service

--- vpc.tf
+++ vpc.tf
@@ -35,3 +35,8 @@ resource "aws_subnet" "public" {
     Name = format("Public_Subnet-%d", count.index)
   }
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = data.aws_vpc.default_vpc.id
+  service_name = "com.amazonaws.us-east-1.dynamodb"
+}
