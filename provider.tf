ec82aa1d8acaf6b6ef3e62033f4a50f7e0a74d5d
JK-Chung <chungjeeken@outlook.com>
2022-06-07 16:36:32 +0100

    Removing now-deleted credentials for networking-infrastructure

--- provider.tf
+++ provider.tf
@@ -3,7 +3,6 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = "~> 4.8"
-      configuration_aliases = [aws.networking-infrastructure]
     }
   }
 
@@ -25,30 +24,6 @@ provider "aws" {
   default_tags {
     tags = local.default_tags
   }
-}
-
-provider "aws" {
-  alias  = "networking-infrastructure"
-  region = "eu-west-1"
-
-  access_key = var.NETWORKING_AWS_ACCESS_KEY_ID
-  secret_key = var.NETWORKING_AWS_SECRET_ACCESS_KEY
-
-  default_tags {
-    tags = local.default_tags
-  }
-}
-
-# AWS Networking Credentials
-variable "NETWORKING_AWS_ACCESS_KEY_ID" {
-  type        = string
-  description = "The AWS Access Key ID for Networking-Infrastructure account"
-}
-
-variable "NETWORKING_AWS_SECRET_ACCESS_KEY" {
-  type        = string
-  description = "The AWS Access Secret Key for Networking-Infrastructure account"
-  sensitive   = true
 }
 
 data "aws_region" "current" {}
