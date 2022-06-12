b86f64587449b1dc8711c83fab04eb3b100eebae
JK-Chung <chungjeeken@outlook.com>
2022-06-12 20:44:33 +0100

    Using new DNS infra

--- route53.tf
+++ route53.tf
@@ -1,15 +1,10 @@
-resource "aws_route53_zone" "projects" {
-  for_each = local.env_root_domain
-  name     = each.value
-}
-
 module "route53_subdomains" {
   for_each = local.env_root_domain
   source   = "./route53_subdomains"
 
-  env_root_domain = each.value
+  env_root_domain = a.env_root_domain
   subdomains      = [for r in local.elb_routable_apps : r.subdomain if r.env_root_domain == each.value]
-  route53_zone_id = aws_route53_zone.projects[each.value].zone_id
+  route53_zone_id = aws_ssm_parameter.r53_zoneids[each.value.env_root_domain].value
 
   to_alias_to = {
     name    = aws_lb.common_lb.dns_name
--- route53_cross_account/data.tf
+++
@@ -1,4 +0,0 @@
-data "tfe_outputs" "cross_account-route53" {
-  organization = "jkc-projects"
-  workspace    = "cross-account--dns-infrastructure"
-}
