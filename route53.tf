aa8d5b1d13e5a00d5dd6fbc832341adefe9ae338
JK-Chung <chungjeeken@outlook.com>
2022-06-12 20:55:38 +0100

    Creating records for each subdomain/fqdn

--- route53.tf
+++ route53.tf
@@ -1,13 +1,13 @@
-module "route53_subdomains" {
-  for_each = local.env_root_domain
-  source   = "./route53_subdomains"
+resource "aws_route53_record" "subdomain_alias_records" {
+  for_each = local.elb_routable_apps
 
-  env_root_domain = a.env_root_domain
-  subdomains      = [for r in local.elb_routable_apps : r.subdomain if r.env_root_domain == each.value]
-  route53_zone_id = aws_ssm_parameter.r53_zoneids[each.value.env_root_domain].value
+  name    = each.value.fqdn
+  type    = "A"
+  zone_id = aws_ssm_parameter.r53_zoneids[each.value.env_root_domain].value
 
-  to_alias_to = {
-    name    = aws_lb.common_lb.dns_name
-    zone_id = aws_lb.common_lb.zone_id
+  alias {
+    name                   = aws_lb.common_lb.dns_name
+    zone_id                = aws_lb.common_lb.zone_id
+    evaluate_target_health = true
   }
 }
